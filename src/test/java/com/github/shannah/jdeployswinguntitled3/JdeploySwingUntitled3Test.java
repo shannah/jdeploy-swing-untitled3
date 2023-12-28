8be342740e708529fd4405c90fb94634094686b2
steve <steve@host.docker.internal>
2023-12-28 06:49:16 -0800

    Commit message goes here

---
+++ src/test/java/com/github/shannah/jdeployswinguntitled3/JdeploySwingUntitled3Test.java
@@ -0,0 +1,38 @@
+package com.github.shannah.jdeployswinguntitled3;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JdeploySwingUntitled3Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JdeploySwingUntitled3Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JdeploySwingUntitled3Test.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
